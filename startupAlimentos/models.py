26e3c2e913d816f04082271bfeff740cec0ab286
Luis H. P. Mendes <luishpmendes@gmail.com>
2017-03-28 18:38:30 -0300

    atualizando modelos

--- startupAlimentos/models.py
+++ startupAlimentos/models.py
@@ -3,12 +3,11 @@ from django.db import models
 
 class Ingrediente(models.Model):
     nome = models.CharField(max_length=100)
-    valor = models.DecimalField(max_digits=5, decimal_places=2)
+    valor = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
 
 class Lanche(models.Model):
     nome = models.CharField(max_length=100)
     ingredientes = models.ManyToManyField(Ingrediente)
-    valor = models.DecimalField(max_digits=10, decimal_places=2)
 
 class Promocao(models.Model):
     nome = models.CharField(max_length=100)
