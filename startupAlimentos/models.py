a4b6d6aee8b03427ad930bcd6becd6cc5562e277
Luis H. P. Mendes <luishpmendes@gmail.com>
2017-03-28 22:33:08 -0300

    populando com lanches promocionais

--- startupAlimentos/models.py
+++ startupAlimentos/models.py
@@ -23,6 +23,30 @@ class Lanche(models.Model):
     ingredientes = models.ManyToManyField(Ingrediente, through='LancheIngrediente')
     def __str__(self):
         return self.nome
+    def valor(self):
+        result = 0
+        muitaCarneAtiva = False
+        if (len(Promocao.objects.filter(nome="Muita carne")) > 0): # se existe a promoção Muita carne
+            muitaCarne = Promocao.objects.filter(nome="Muita carne")[0]
+            muitaCarneAtiva = muitaCarne.esta_ativa
+        muitoQueijoAtiva = False
+        if (len(Promocao.objects.filter(nome="Muita queijo")) > 0): # se existe a promoção Muita queijo
+            muitoQueijo = Promocao.objects.filter(nome="Muita queijo")[0]
+            muitoQueijoAtiva = muitaCarne.esta_ativa
+        for ingrediente in self.ingredientes.all():
+            lanche_ingrediente = LancheIngrediente.objects.get(lanche=self, ingrediente=ingrediente)
+            result += lanche_ingrediente.quantidade * ingrediente.valor
+            if (ingrediente.nome == "Hambúrguer de carne" and muitaCarneAtiva):
+                result -= (lanche_ingrediente.quantidade // 3) * ingrediente.valor
+            if (ingrediente.nome == "Queijo" and muitoQueijoAtiva):
+                result -= (lanche_ingrediente.quantidade // 3) * ingrediente.valor
+        if (len(Promocao.objects.filter(nome="Light")) > 0): # se existe a promoção Light
+            light = Promocao.objects.filter(nome="Light")[0]
+            if (light.esta_ativa): # se a promoção Light está ativa
+                if (len(self.ingredientes.filter(nome="alface")) > 0 and self.ingredientes.filter(nome="alface")[0].quantidade > 0): # se o lanche possui alface
+                    if (len(self.ingredientes.filter(nome="bacon")) <= 0 or self.ingredientes.filter(nome="bacon")[0].quantidade <= 0): # se o lanche não possui bacon
+                        result = 0.9 * result
+        return result
 
 class LancheIngrediente(models.Model):
     lanche = models.ForeignKey(Lanche, on_delete=models.CASCADE)
